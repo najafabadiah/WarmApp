d2bdf7bbac98ad123b9ad9fb3300896855e1fe15
AmirHossein Nadjafabadi Farahani <najafabadi.a.h@gmail.com>
2021-05-30 18:39:57 +0200

    change main page

--- App.js
+++ App.js
@@ -2,11 +2,38 @@
  * This start the app navigation
  */
 
-import * as React from 'react';
-import 'react-native-gesture-handler';
-import Navigation from './src/navigation/authentification/index';
-const App = () => {
-  return <Navigation />;
-};
-
-export default App;
+ import React from "react";
+ import { Button, Text, TextInput, View, StyleSheet } from "react-native";
+ 
+ const App = () => {
+   const [message, setMessage] = React.useState();
+ 
+   return (
+     <View>
+       <Button
+         title="Say Hello"
+         onPress={() => {
+           setTimeout(() => {
+             setMessage("Hello Tester");
+           }, Math.floor(Math.random() * 200));
+         }}
+       />
+       {message && (
+         <Text style={styles.messageText} testID="printed-message">
+           {message}
+         </Text>
+       )}
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   messageText: {
+     fontFamily: "Arial",
+     fontSize: 38,
+     textAlign: "center",
+     marginTop: 10
+   }
+ });
+ 
+ export default App;
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,14 +2,20 @@
  * A mock of the firebase node modules is required
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
-import renderer from 'react-test-renderer';
-
-jest.mock('../node_modules/firebase')
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+ import "react-native";
+ import React from "react";
+ import App from "../App";
+ 
+ import { fireEvent, render, waitFor } from "@testing-library/react-native";
+ 
+ it("Renders Message", async () => {
+   const { getByTestId, getByText, queryByTestId, toJSON } = render(<App />);
+ 
+   const button = getByText("Say Hello");
+   fireEvent.press(button);
+ 
+   await waitFor(() => expect(queryByTestId("printed-message")).toBeTruthy());
+ 
+   expect(getByTestId("printed-message").props.children).toBe("Hello Tester");
+   expect(toJSON()).toMatchSnapshot();
+ });
